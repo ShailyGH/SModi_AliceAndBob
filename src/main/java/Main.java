a232db647ba00e89d3e4a81b21734b2e25ee5128
ShailyGH <shailykunal@.com>
2024-02-29 17:15:13 -0500

    Finished

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,23 +1,41 @@
 /**
- * Created by iyasuwatts on 10/17/17.
- * Modified by kristofer on Jan 2024.
+ * Created by Shaily Modi on 02/28/2024
  */
+
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args ){
         // create a new object of class Main
+        Main main = new Main();
 
         // call the compute() method on that new object
+        main.compute();
     }
 
     public void compute() {
         // Get input string of name from user
-        
-        // Create conditional to check name against Alice and Bob
+        boolean isNameValid = false;
+        Scanner scanner = new Scanner(System.in);
 
+        System.out.print("Enter your name: ");
+        String name = scanner.nextLine();
+
+        // Create conditional to check name against Alice and Bob
+        if (name.equals("Alice") || name.equals("Bob"))
+        {
+            isNameValid = true;
+        }
         // Print greeting to screen if Alice or Bob are true
-        
-        // Print "You are neither Alice nor Bob." otherwise
+        if (isNameValid)
+        {
+            System.out.println("Hello, " + name + "!");
+        }
+        else
+        {
+            System.out.println("You are neither Alice nor Bob.");
+        }
 
     }
 }
